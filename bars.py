48f7a75bef3faa16138a2668b5b2f0689d432e75
veean <nersesowm@gmail.com>
2016-10-10 15:35:57 +0400

    fix functions

--- bars.py
+++ bars.py
@@ -2,10 +2,10 @@ import json
 import os
 
 
-def load_data(filepath):
+def load_data(path_file):
     if not os.path.exists(filepath):
         return None
-    with open(filepath, 'r', encoding="utf-8") as file_handler:
+    with open(path_file, 'r', encoding="utf-8") as file_handler:
         return json.load(file_handler)
 
 
@@ -20,45 +20,43 @@ def get_biggest_bar(data):
 
 
 def get_smallest_bar(data):
-    smallest_bar_name = ''
+    smallest_bar_name = []
     smallest_bar = float('inf')
     for bars in data:
-        if bars['Cells']['SeatsCount'] >= smallest_bar:
+        if bars['Cells']['SeatsCount'] <= smallest_bar:
             smallest_bar = bars['Cells']['SeatsCount']
-            biggest_bar_name = bars['Cells']['Name']
+            smallest_bar_name = bars['Cells']['Name']
     return smallest_bar_name
 
 
 def get_closest_bar(data, longitude, latitude):
     min_distance = float('inf')
-    longitude_closest = 0.0
-    latitude_closest = 0.0
     name_bar = ''
     for name in data:
-        distance = (longitude - name['Cells']['geoData']['coordinates'][0]) ** 2 + (latitude - name['Cells']['geoData']['coordinates'][1]) ** 2
+        distance = (longitude - name['Cells']['geoData']['coordinates'][0]) ** 2 + (latitude - name['Cells']['geoData'][
+            'coordinates'][1]) ** 2
         if distance < min_distance:
-            longitude_closest = name['Cells']['geoData']['coordinates'][0]
-            latitude_closest = name['Cells']['geoData']['coordinates'][1]
+            min_distance = distance
             name_bar = name['Cells']['Name']
 
     return name_bar
-    # return name_bar, longitude_closest, latitude_closest
 
 
 if __name__ == '__main__':
-     while True:
+    while True:
         filepath = input('Введите путь до json файла: ')
 
         print('Введите координаты своего местоположения')
-        longitude = float(input('Долгота :'))
-        latitude = float(input('Широта:'))
 
-        if not filepath and longitude and latitude:
+        user_longitude = float(input('Долгота :'))
+        user_latitude = float(input('Широта:'))
+
+        if not filepath and user_longitude and user_latitude:
             print('Try again!')
         else:
             try:
                 pretty_data = load_data(filepath)
-                print('Ближайший бар:', get_closest_bar(pretty_data, longitude, latitude))
+                print('Ближайший бар:', get_closest_bar(pretty_data, user_longitude, user_latitude))
 
                 print('Зе biggest бар: ', get_biggest_bar(pretty_data))
                 print('Зе smallest бар: ', get_smallest_bar(pretty_data))
