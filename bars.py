12424a0f7d5f27597876fd1d195bb8bfc28dc269
veean <nersesowm@gmail.com>
2016-11-02 01:58:41 +0400

    some updates in small\biggest bar functions (with std methods)

--- bars.py
+++ bars.py
@@ -1,9 +1,10 @@
 import json
 import os
+import argparse
 
 
 def load_data(path_file):
-    if not os.path.exists(filepath):
+    if not os.path.exists(path_file):
         return None
     with open(path_file, 'r', encoding="utf-8") as file_handler:
         return json.load(file_handler)
@@ -14,44 +15,42 @@ def get_biggest_bar(data):
     return bar['Cells']['Name']
 
 
-
 def get_smallest_bar(data):
     bar = min(data, key=lambda x: x['Cells']['SeatsCount'])
     return bar['Cells']['Name']
 
 
+def count_distance(data_unit, longitude, latitude):
+    distance = (longitude - data_unit['Cells']['geoData']['coordinates'][0]) ** 2 + (latitude - data_unit['Cells'][
+        'geoData']['coordinates'][1]) ** 2
+    return distance
 
-def get_closest_bar(data, longitude, latitude):
-    min_distance = float('inf')
-    name_bar = ''
-    for name in data:
-        distance = (longitude - name['Cells']['geoData']['coordinates'][0]) ** 2 + (latitude - name['Cells']['geoData'][
-            'coordinates'][1]) ** 2
-        if distance < min_distance:
-            min_distance = distance
-            name_bar = name['Cells']['Name']
 
-    return name_bar
+def get_closest_bar_subver(data, longtitude, latitude):
+
+    bar = min(data, key=lambda x: count_distance(x, float(longtitude), float(latitude)))
+    return bar['Cells']['Name']
 
 
 if __name__ == '__main__':
-    while True:
-        filepath = input('Введите путь до json файла: ')
 
-        print('Введите координаты своего местоположения')
+    parser = argparse.ArgumentParser(description="Analysin lots of Moscow bars")
+    parser.add_argument('-file', help='Путь к json файлу')
+    parser.add_argument('-ln', help="Ваши координаты : текущая широта")
+    parser.add_argument('-lt', help="Ваши координаты : текущая долгота")
+    args = parser.parse_args()
 
-        user_longitude = float(input('Долгота :'))
-        user_latitude = float(input('Широта:'))
+    filepath = str(args.file).encode('ascii').decode('utf-8')
 
-        if not filepath and user_longitude and user_latitude:
-            print('Try again!')
-        else:
-            try:
-                pretty_data = load_data(filepath)
-                print('Ближайший бар:', get_closest_bar(pretty_data, user_longitude, user_latitude))
+    if not args.file:
+        print('Try again with arguments!')
+    else:
+        try:
+            pretty_data = load_data(filepath)
+            print('Зе biggest бар: ', get_biggest_bar(pretty_data))
+            print('Зе smallest бар: ', get_smallest_bar(pretty_data))
+            if args.ln and args.lt:
+                print('Ближайший бар:', get_closest_bar_subver(pretty_data, args.ln, args.lt))
 
-                print('Зе biggest бар: ', get_biggest_bar(pretty_data))
-                print('Зе smallest бар: ', get_smallest_bar(pretty_data))
-                break
-            except IOError as e:
-                print("Try better : {}".format(e.args[1]))
+        except FileNotFoundError as e:
+            print("Try better : {}".format(e.args[1]))
